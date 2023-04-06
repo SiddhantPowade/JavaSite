14469b67de0066ada8edcb6ff840b6277f567f04
SiddhantPowade <129779123+SiddhantPowade@users.noreply.github.com>
2023-04-06 20:20:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,150 @@
+function myFunction() {
+  var x = document.getElementById("content-box-of-java");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction2() {
+  var x = document.getElementById("dataContent-002");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction3() {
+  var x = document.getElementById("dataContent-003");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction4() {
+  var x = document.getElementById("dataContent-004");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction5() {
+  var x = document.getElementById("dataContent-005");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction6() {
+  var x = document.getElementById("dataContent-006");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction7() {
+  var x = document.getElementById("dataContent-007");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction8() {
+  var x = document.getElementById("dataContent-008");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction9() {
+  var x = document.getElementById("dataContent-009");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction10() {
+  var x = document.getElementById("dataContent-0010");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction11() {
+  var x = document.getElementById("dataContent-0011");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction12() {
+  var x = document.getElementById("dataContent-0012");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction13() {
+  var x = document.getElementById("dataContent-0013");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction14() {
+  var x = document.getElementById("dataContent-0014");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction15() {
+  var x = document.getElementById("dataContent-0015");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction16() {
+  var x = document.getElementById("dataContent-0016");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction17() {
+  var x = document.getElementById("dataContent-0017");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction18() {
+  var x = document.getElementById("aside-content-01");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
